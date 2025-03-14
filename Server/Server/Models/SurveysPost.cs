80b7fa63db94065e545ec1e686994e565d01bd17
Sarah Murphree <104289709+smurph7894@users.noreply.github.com>
2025-03-13 22:24:39 -0700

    update model

---
+++ Server/Server/Models/SurveysPost.cs
@@ -0,0 +1,18 @@
+﻿namespace Server.Models
+{
+    public class SurveysPost
+    {
+        public int surveyId { get; set; }
+        public int userId { get; set; }
+        public int commentId { get; set; }
+        private int idCounter;
+
+        public SurveysPost(int userId, int commentId)
+        {
+            idCounter++;
+            this.surveyId = idCounter;
+            this.userId = userId;
+            this.commentId = commentId;
+        }
+    }
+}
