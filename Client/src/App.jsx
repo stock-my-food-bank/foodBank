afd40e9a0904e9b4763a4b3266c24c864e1b75a0
Sarah Murphree <104289709+smurph7894@users.noreply.github.com>
2025-03-20 14:33:55 -0700

    useEffect and getting foodItemList

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,18 +7,6 @@ import ResultsPage from './Components/RPComponents/ResultsPage';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  /*working call to server and sqlite info -Sarah
-  always add a , [] as second param in useeffect so it's only called once
-  */
-  useEffect(()=> {
-    // fetch('https://localhost:7183/api/Users').then(res=>{
-    //   res.json().then(json=> {console.log(json)
-    //     setCount(json)
-    //   })
-    // })
-  }, [])
 
   return (
 
--- Client/src/Components/SPComponents/SurveyPage.jsx
+++ Client/src/Components/SPComponents/SurveyPage.jsx
@@ -1,6 +1,5 @@
 // Needed for routes as we are importing from the library
 import { Link } from 'react-router-dom';
-
 //Nyambura --Imported Basic Button Styling  
 import BasicButton from '../BasicButton'
 //Nyambura --Imported Food Item list 
@@ -13,66 +12,101 @@ import Logo from '../Logo';
 import Pagination from '../Pagination';
 //Imported PopUp 
 import Popup from './Popup';
+import { useEffect, useState } from 'react';
 
 function SurveyPage() {
+    const [foodItemList, setFoodItemsList] = useState();
+    const [comment, setComment] = useState();
+    const [indivSurveyResults ,setIndivSurveyResults] = useState();
+    
+    /*working call to server and sqlite info -Sarah
+    always add a , [] as second param in useeffect so it's only called once
+    */
+   //Murphree - calls for foodItems
+    useEffect(()=>{
+        GetFoodList();
+    }, []);
 
-    //useEffect
+    async function GetFoodList(){
+        const url = 'htts://localhost:7183/api/FoodItems';
+        try {
+            const response = await fetch(url);
+            if(!response.ok){
+                throw new Error(`Response Status: ${repsonse.status}`);
+            }
+            const json = await response.json();
+            setFoodItemsList(json);
+            console.log(foodItemList);
+        } catch (error){
+            console.error(error.message);
+        }
+    }
+
+    // async function GetComment = async() => {}
+    
+
+    // const SubmitSurveyHandler = async(comment, indivSurveyResults) =>{
+    //     const response = await fetch('https://localhost:7183/api/FoodItems', {
+    //         method: "POST",
+    //         body: JSON.stringify({key: "example"}),
+    //         //
+    //         }
+    //     )
+    // }
 
     //popup controls
 
-    // submit handler (votes & comment)
-  return (
+    return (
     
   
-    <div className="Container-fluid "> 
-        {/* Header  */}
-       <div>
-            <Logo/>
-       </div>
-
-        {/* Results Button for Survey Page, styled to start from opposite side */}
-        <div className="d-flex flex-row-reverse">
-            <BasicButton text="Results"/>
-        </div>
-
-        {/* StockMyFoodBank Header  */}
-        <h1>
-            StockMyFoodBank
-        </h1>
-
-        {/* Container that hold view 1 survey page food items */}
-        <div className="Container-md Stock-Color">
-            <FoodItems
-            _submitHandler={submitHandler}
-            />
-        </div>
-        {/* Pagination - SM removing for now for prototype */}
+        <div className="Container-fluid "> 
+            {/* Header  */}
         <div>
-            <Pagination />
-
+                <Logo/>
         </div>
 
-        {/* Comments section added for view 1 survey page  */}
-        <div>
-            <Comments />
-        </div>
+            {/* Results Button for Survey Page, styled to start from opposite side */}
+            <div className="d-flex flex-row-reverse">
+                <BasicButton text="Results"/>
+            </div>
 
-        {/* Submit Button for Survey Page, styled to be in the middle of page */}
-        <div className="d-flex justify-content-center ">
-          <Link to="/results"> <BasicButton text="Submit"/></Link>
-        </div>
-        {/* Popup modal can alter header and text using props to show error and submitted popup when submit button is clicked  */}
-        <div>
-            <Popup/>
-        </div>
+            {/* StockMyFoodBank Header  */}
+            <h1>
+                StockMyFoodBank
+            </h1>
 
-        {/* Footer for view 1 survey page */}
-        <div>
-            <Logo />
-       </div>              
-    </div>
-    
-    
+            {/* Container that hold view 1 survey page food items */}
+            <div className="Container-md Stock-Color">
+                <FoodItems 
+                foodItemList = {foodItemList}
+                // {/* // _submitHandler={submitHandler} */}
+                />
+            </div>
+            {/* Pagination - SM removing for now for prototype */}
+            <div>
+                <Pagination />
+
+            </div>
+
+            {/* Comments section added for view 1 survey page  */}
+            <div>
+                <Comments />
+            </div>
+
+            {/* Submit Button for Survey Page, styled to be in the middle of page */}
+            <div className="d-flex justify-content-center ">
+            <Link to="/results"> <BasicButton text="Submit"/></Link>
+            </div>
+            {/* Popup modal can alter header and text using props to show error and submitted popup when submit button is clicked  */}
+            <div>
+                <Popup/>
+            </div>
+
+            {/* Footer for view 1 survey page */}
+            <div>
+                <Logo />
+        </div>
+        </div>
   );
 }
 
